0f258936c07fb9a811814700d6f9af15cb19fbae
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-06 16:15:42 -0700

    Added pet details and edit page.

--- app.py
+++ app.py
@@ -72,5 +72,36 @@ def add_pet_form():
         return render_template(
             "add_pet.html", form=form)
 
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def handle_edit_pet_form(pet_id):
+    """ shows and handles the edit pet form """
 
+    pet = Pet.query.get_or_404(pet_id)
+    form = AddPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data or ''
+        age = form.age.data
+        notes = form.notes.data or ''
+
+
+        # do stuff with data/insert to db
+        new_pet = Pet(
+            name=name,
+            species=species,
+            photo_url=photo_url,
+            age=age,
+            notes=notes)
+
+        db.session.add(new_pet)
+        db.session.commit()
+
+        flash(f"Added {name} to adoptable pets")
+        return redirect("/")
+
+    else:
+        return render_template(
+            "pet_details.html", form=form, pet=pet)
 
