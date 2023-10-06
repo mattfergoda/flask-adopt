de8286902f85d8c1159dc7d595e14284eaa40db0
Matt Fergoda <mattfergoda@users.noreply.github.com>
2023-10-06 12:25:30 -0700

    Added add pet form class.

--- forms.py
+++ forms.py
@@ -1,1 +1,19 @@
-"""Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding pets."""
+
+    name = StringField("Pet Name")
+    species = StringField("Species")
+    photo_url = StringField("Pet Photo URL")
+    age = SelectField('Pet Age Category',
+        choices=[
+            ('baby', 'Baby'),
+            ('young', 'Young'),
+            ('adult', 'Adult'),
+            ('senior', 'Senior'),
+                 ])
+    notes = TextAreaField("Anything you want us to know?")
+    
